31a1b427f6cbfcc01aaa0c49f1407f6b327c0980
mzemskov <mzemskov@dimax.digital>
2022-12-09 19:05:34 +0300

    task_1_done

---
+++ Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Введите количество чисел:");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[n];
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+Console.WriteLine($"Введите {i+1} число");
+int b = Convert.ToInt32(Console.ReadLine());
+numbers[i] = b;
+if (b > 0)
+count++;
+}
+Console.WriteLine($"Вы ввели {count} чисел больше 0");
